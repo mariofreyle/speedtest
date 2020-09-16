f527063c62c03ae69284973a8ef2d3726f764e6b
mariofreyle <35880371+mariofreyle@users.noreply.github.com>
2020-09-16 18:32:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1331,11 +1331,11 @@ function TestStage(props) {
 
             //instant.results.push(!transfer.transferred && prev.instantSpeed ? (instant.speed + prev.instantSpeed) / 2 : instant.speed);
             instant.results.push(instant.speed);
-            instant.maxResults = (loadTime > 2500 ? 5 : 5) + Math.round(transfer.average.time / 80);
+            instant.maxResults = (loadTime > 1000 ? 5 : 3) + Math.round(transfer.average.time / 80);
             instant.maxResults = instant.maxResults > 10 ? 10 : instant.maxResults;
-            if (instant.results.length > (loadTime > 1000 ? 5 : 3)) {
-                //instant.results.splice(0, instant.results.length - instant.maxResults);
-                instant.results.splice(0, 1);
+            if (instant.results.length > instant.maxResults) {
+                instant.results.splice(0, instant.results.length - instant.maxResults);
+                //instant.results.splice(0, 1);
             }
 
             average.speed = countArrayItems(instant.results) / instant.results.length;
