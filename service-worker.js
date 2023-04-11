08e5043396a97f916f3481a648821ef9e35bb23f
Luis Freyle <35880371+mariofreyle@users.noreply.github.com>
2023-04-11 00:53:21 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,11 +18,31 @@
     });
 
     self.addEventListener("activate", function(){
-        console.log("Activated!");
+        console.log("Worker activated!");
     });
     
     self.addEventListener("fetch", function(e){
         if(e.request.mode === "navigate"){
+            if(e.request.url.indexOf("refresh=1") != -1){
+                async function respond(){
+                    try {
+                        var response = await fetch(e.request),
+                            cache;
+                        if(response.ok){
+                            cache = await caches.open(CACHE_NAME);
+                            cache.put(e.request, response.clone());
+                        }
+                        return response;
+                    }catch(error){
+                        
+                    }
+                    return await caches.match(e.request, {ignoreSearch: true});
+                }
+                e.respondWith(
+                    respond()
+                );
+                return;
+            }
             e.respondWith(
                 caches.match(e.request, {ignoreSearch: true}).then(function(res){
                     if(res){
