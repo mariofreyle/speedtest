baf53056b4edfb151a305fb2b37a76fb52da60bd
Luis Freyle <35880371+mariofreyle@users.noreply.github.com>
2023-04-12 19:19:11 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -26,31 +26,27 @@
         if(e.request.mode === "navigate"){
             e.respondWith(
                 caches.open(CACHE_NAME).then(function(cache){
-                    return cache.match(e.request, {ignoreSearch: true}).then(function(res){
-                        if(res && (e.request.url.indexOf("refresh=1") != -1 || location.search.indexOf("store=0") != -1 || location.hostname == "ispeedtest.epizy.com")){
+                    return cache.match(e.request, {ignoreSearch: true}).then(function(cached){
+                        if(!cached){
+                            return fetch(e.request);
+                        }
+                        if(e.request.url.indexOf("refresh=1") != -1 || location.search.indexOf("store=0") != -1 || location.hostname == "ispeedtest.epizy.com"){
                             var url = e.request.url.replace(/#(.*)/, "");
                             url += (url.indexOf("?") == -1 ? "?" : "&") + "v=" + Math.random();
-                            return fetch(url).then(function(response){
-                                if(response.ok){
+                            return fetch(url).then(function(fetched){
+                                if(fetched.ok){
                                     cache.delete(e.request, {ignoreSearch: true}).then(function(status){
-                                        console.log(status);
+                                        console.log("cache item deleted: " + status);
                                     });
-                                    cache.put(e.request, response.clone());
-                                    cache.matchAll(e.request, {ignoreSearch: true}).then(function(matchs){
-                                        console.log(matchs);
-                                    });
-                                    return response;
+                                    cache.put(e.request, fetched.clone());
+                                    return fetched;
                                 }
-                                return res;
+                                return cached;
                             }).catch(function(){
-                                return res;
+                                return cached;
                             });
                         }
-                        if(res){
-                            return res;
-                        }
-
-                        return fetch(e.request);
+                        return cached;
                     })
                 })
             );
